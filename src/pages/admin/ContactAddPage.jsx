f447428bbcc5996c3bd29adb042b1861435e1644
seogy <seogyeonghwan48@gmail.com>
2025-02-02 22:40:13 +0900

    관리자 ContactAddPage 추가

--- src/pages/admin/ContactAddPage.jsx
+++ src/pages/admin/ContactAddPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MainTitle from '../../components/admin/MainTitle'
+import ContactAddProviderForm from '../../components/user/ContactAddProviderForm'
 
 function ContactAddPage() {
     return (
-        <div className='pt-20'>
+        <div className='pt-20 bg-white'>
             <MainTitle content={"Contact Add"} button={"목록"} path={"../contact"} />
+            <div className='px-1 md:px-4 lg:px-7'>
+                <ContactAddProviderForm />
+            </div>
         </div>
     )
 }
